5185c1900668163cf25f85b87bbbb4b4f227f079
Riday ♡ <ridayshah2001@gmail.com>
2020-12-26 13:13:00 +0530

    Feat: styled component for nav links

---
+++ client/src/assets/styles/navLink.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { colors, highlights } from '../theme/constants';
+
+type Props = {
+	dark?: boolean;
+	active?: boolean;
+};
+
+const NavLink = styled.a<Props>`
+	color: ${({ active, dark }) => (active ? colors.white1 : dark ? colors.pink : colors.azure)};
+	background-color: ${({ active, dark }) => (active ? (dark ? highlights.pink : highlights.blue) : 'none')};
+	text-decoration: ${({ active }) => (active ? 'none' : 'underline')};
+	padding: 5px 10px;
+
+	:hover {
+		color: ${colors.white1};
+		background-color: ${({ dark }) => (dark ? highlights.pink : highlights.blue)};
+		text-decoration: none;
+	}
+
+	@media (max-width: 768px) {
+		width: 100%;
+		display: block;
+		padding: 7.5% 1em;
+		margin: 12% auto;
+		text-align: center;
+		text-decoration: none;
+	}
+`;
+
+export default NavLink;
