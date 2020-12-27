4dd554db74f5845982011354e4b36cfd7b9f04e8
Riday ♡ <ridayshah2001@gmail.com>
2020-12-27 12:46:47 +0530

    Feat: first attempt at the hero section

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,15 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, Fragment } from 'react';
 import Nav from '../components/nav';
 import HeroSection from '../assets/styles/heroSection';
+import Avatar from '../assets/styles/avatar';
+import HeroStatement from '../assets/styles/heroStatement';
 import ThemeContext from '../assets/theme/themeContext';
+import pfp from '../assets/images/me.jpg';
 
 const Home = () => {
 	const { dark } = useContext(ThemeContext);
 	return (
-		<div>
+		<Fragment>
 			<Nav active='home' />
-			<HeroSection dark={dark}>This is the Hero Section on the home page.</HeroSection>
-		</div>
+			<HeroSection dark={dark} className='row d-flex align-items-md-center justify-content-center mx-auto mt-2'>
+				<div className='col-md-4 p-5 text-center'>
+					<Avatar src={pfp} alt='My Profile Picture.' dark={dark} />
+				</div>
+				<HeroStatement className='col-md-6' dark={dark}>
+					Hi, I am Riday. I am a self-taught Web Developer. My favorite tools of choice are Nodejs and
+					Typescript. I strive to make effective use of my expertise to develop things that matter.
+				</HeroStatement>
+			</HeroSection>
+		</Fragment>
 	);
 };
 
