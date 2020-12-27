3b66cd3f2d31141a25f064b6d69050165e22a3f4
Riday ♡ <ridayshah2001@gmail.com>
2020-12-27 13:39:25 +0530

    Style: responsive design for xs devices

--- client/src/assets/styles/avatar.ts
+++ client/src/assets/styles/avatar.ts
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
-import { colors } from '../theme/constants';
+import { breakpoints, colors, shadows } from '../theme/constants';
 
 const Avatar = styled.img<{ dark?: boolean }>`
 	border-radius: 50%;
 	width: 75%;
 	border: solid 7.5px ${({ dark }) => (dark ? colors.pink : colors.azure)};
+	box-shadow: ${({ dark }) => (dark ? 'none' : shadows.layer2)};
 
 	animation-name: floating;
 	animation-duration: 3.5s;
 	animation-iteration-count: infinite;
 	animation-timing-function: ease-in-out;
 
-	@media (min-width: 768px) {
+	@media (min-width: ${breakpoints.md}) {
 		width: 50%;
 	}
 
@@ -24,6 +25,20 @@ const Avatar = styled.img<{ dark?: boolean }>`
 		}
 		to {
 			transform: translate(0, 0px);
+		}
+	}
+
+	@media (max-width: ${breakpoints.xs}) {
+		@keyframes floating {
+			from {
+				transform: translate(0, 0px);
+			}
+			50% {
+				transform: translate(0, -25px);
+			}
+			to {
+				transform: translate(0, 0px);
+			}
 		}
 	}
 `;
