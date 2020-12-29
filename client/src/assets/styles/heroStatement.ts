02492fbbcadb26e6a4cbca658b8344cbd8f1ea3c
Riday ♡ <ridayshah2001@gmail.com>
2020-12-29 18:21:58 +0530

    Feat: use mobile first design

--- client/src/assets/styles/heroStatement.ts
+++ client/src/assets/styles/heroStatement.ts
@@ -3,23 +3,25 @@ import { breakpoints, colors, shadows } from '../theme/constants';
 
 const HeroStatement = styled.div<{ dark?: boolean }>`
 	cursor: default;
-	font-size: 1.25em;
 	box-shadow: ${({ dark }) => (dark ? 'none' : shadows.layer2)};
 	background-color: ${({ dark }) => (dark ? colors.dark2 : 'inherit')};
 	padding: 1.25em;
 	border-radius: 14px;
+	font-size: 0.75rem;
+	margin-top: -1rem;
 
-	@media (min-width: 768px) {
-		font-size: 2em;
-	}
-
-	@media (max-width: 768px) {
+	@media (min-width: ${breakpoints.xs}) {
 		margin-top: 0.5rem;
+		font-size: 1.15em;
 	}
 
-	@media (max-width: ${breakpoints.xs}) {
-		margin-top: -1rem;
-		font-size: 0.8rem;
+	@media (min-width: ${breakpoints.sm}) {
+		font-size: 1.25em;
+	}
+
+	@media (min-width: ${breakpoints.md}) {
+		font-size: 2em;
+		margin-top: 0;
 	}
 `;
 
