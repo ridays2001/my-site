fae9529d250096f15402336abcafbe26eb7f0a35
Riday ♡ <ridayshah2001@gmail.com>
2020-12-30 12:20:47 +0530

    Feat: added empty pages for about, blog, and projects route

---
+++ client/src/routes/about.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const About = () => <div>About Me Page</div>;
+
+export default About;
---
+++ client/src/routes/blog.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Blog = () => <div>Blog Home Page</div>;
+
+export default Blog;
