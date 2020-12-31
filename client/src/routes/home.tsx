bdad0f6269c69d2a9aa8ccc1935a908a64aef04e
Riday ♡ <ridayshah2001@gmail.com>
2020-12-31 18:55:24 +0530

    Refactor: card styles

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -6,6 +6,7 @@ import HeroStatement from '../assets/styles/heroStatement';
 import ProjectSection from '../assets/styles/projectSection';
 import ThemeContext from '../assets/theme/themeContext';
 import Nav from '../components/nav';
+import { BackEnd, FrontEnd } from '../util/icons';
 import projects from '../util/projects';
 
 const Home = () => {
@@ -27,44 +28,38 @@ const Home = () => {
 				</HeroStatement>
 			</HeroSection>
 
-			<h2 style={{ marginTop: '10vh' }}>Projects that I worked on</h2>
-			<ProjectSection
-				className='row row-cols-1 row-cols-md-3 row-cols-lg-4 align-items-center'
-				dark={dark ? 1 : 0}
-			>
-				{projects.map(({ back, description, front, future, github, languages, month, name, status }) => (
-					<div className='col' key={name}>
-						<div className='card'>
-							<div className='card-body'>
-								<div className='card-title'>{name}</div>
-								<div className='card-text'>{description}</div>
+			<ProjectSection className='align-items-center' dark={dark ? 1 : 0}>
+				<h2 style={{ marginTop: '10vh' }}>Projects that I worked on</h2>
+				<div className='row row-cols-1 row-cols-md-3 row-cols-lg-4 align-items-center'>
+					{projects.map(({ back, description, front, github, tech, timeline, name, status }) => (
+						<div className='col' key={name}>
+							<div className='card'>
+								<div className='card-body'>
+									<div className='card-title text-start'>{name}</div>
+									<div className='card-subtitle mb-3 text-end'>{timeline}</div>
+									<div className='card-text'>{description}</div>
+								</div>
+								<ul className='list-group list-group-flush text-start'>
+									<li className='list-group-item' key='made-with'>
+										Made With: {tech}
+									</li>
+									<li className='list-group-item' key='status'>
+										{status}
+									</li>
+									<li className='list-group-item' key='front'>
+										<FrontEnd dark={dark} /> {front}
+									</li>
+									<li className='list-group-item' key='back'>
+										<BackEnd dark={dark} /> {back}
+									</li>
+									<li className='list-group-item text-center links' key='gh'>
+										<a href={github}>GitHub ↗</a>
+									</li>
+								</ul>
 							</div>
-							<ul className='list-group list-group-flush text-start'>
-								<li className='list-group-item' key='made-with'>
-									Made With: {languages}
-								</li>
-								<li className='list-group-item' key='timeline'>
-									Timeline: {month}
-								</li>
-								<li className='list-group-item' key='status'>
-									Status: {status}
-								</li>
-								<li className='list-group-item' key='future'>
-									Future Scope: {future}
-								</li>
-								<li className='list-group-item' key='front'>
-									Front End: {front}
-								</li>
-								<li className='list-group-item' key='back'>
-									Back End: {back}
-								</li>
-								<li className='list-group-item text-center' key='gh'>
-									<a href={github}>GitHub ↗</a>
-								</li>
-							</ul>
 						</div>
-					</div>
-				))}
+					))}
+				</div>
 			</ProjectSection>
 		</Fragment>
 	);
