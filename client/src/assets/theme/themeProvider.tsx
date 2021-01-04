c55db2a23083f698e732096583a783f8602d0f10
Riday ♡ <ridayshah2001@gmail.com>
2021-01-04 18:51:31 +0530

    Chore: forward dark prop to bootstrap

--- client/src/assets/theme/themeProvider.tsx
+++ client/src/assets/theme/themeProvider.tsx
@@ -31,7 +31,7 @@ const ThemeProvider = ({ children }: { children: ReactNode | Array<ReactNode> })
 
 	return (
 		<ThemeContext.Provider value={{ dark, toggle }}>
-			<Bootstrap />
+			<Bootstrap dark={dark ? 1 : 0} />
 			<MainStyle dark={dark ? 1 : 0} />
 			{children}
 		</ThemeContext.Provider>
