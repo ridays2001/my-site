ada124fe040cecfc6387c9630a567e73ac873dd8
Riday ♡ <ridayshah2001@gmail.com>
2021-01-06 17:45:16 +0530

    Feat: about page with styled-component

--- client/src/routes/about.tsx
+++ client/src/routes/about.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+import ThemeContext from '../assets/theme/themeContext';
+import Nav from '../components/nav';
+import AboutSection from '../assets/styles/aboutSection';
 
-const About = () => <div>About Me Page</div>;
+const About = () => {
+	const { dark } = useContext(ThemeContext);
+	return (
+		<Fragment>
+			<Nav active='about' />
+			<AboutSection dark={dark ? 1 : 0}>
+				<h2>About Me Page</h2>
+			</AboutSection>
+		</Fragment>
+	);
+};
 
 export default About;
