11188dc4cd787eb47db5bea8824f30bc0901745e
Riday ♡ <ridayshah2001@gmail.com>
2021-01-06 17:45:52 +0530

    Chore: added navbar

--- client/src/routes/blog.tsx
+++ client/src/routes/blog.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
-
-const Blog = () => <div>Blog Home Page</div>;
+import React, { Fragment, useContext } from 'react';
+import ThemeContext from '../assets/theme/themeContext';
+import Nav from '../components/nav';
+const Blog = () => {
+	const { dark } = useContext(ThemeContext);
+	return (
+		<Fragment>
+			<Nav active='blog' />
+			<h2>Blog Home Page</h2>
+		</Fragment>
+	);
+};
 
 export default Blog;
--- client/src/routes/contact.tsx
+++ client/src/routes/contact.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
-
-const Contact = () => <div>Contact Me Page</div>;
+import React, { Fragment, useContext } from 'react';
+import ThemeContext from '../assets/theme/themeContext';
+import Nav from '../components/nav';
+const Contact = () => {
+	const { dark } = useContext(ThemeContext);
+	return (
+		<Fragment>
+			<Nav active='contact' />
+			<h2>Contact Me Page</h2>
+		</Fragment>
+	);
+};
 
 export default Contact;
