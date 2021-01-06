20932db30722cadebf2a905d57292dc3bc55b629
Riday ♡ <ridayshah2001@gmail.com>
2021-01-06 13:07:56 +0530

    Feat: added "see more" button for projects

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import pfp from '../assets/images/me.jpg';
 import Avatar from '../assets/styles/avatar';
 import HeroSection from '../assets/styles/heroSection';
@@ -6,6 +7,7 @@ import ThemeContext from '../assets/theme/themeContext';
 import Nav from '../components/nav';
 import ListProjects from '../components/listProjects';
 import Testimonials from '../components/testimonials';
+import { text } from '../assets/theme/constants';
 
 const Home = () => {
 	const { dark } = useContext(ThemeContext);
@@ -26,7 +28,19 @@ const Home = () => {
 				</article>
 			</HeroSection>
 
+			<h2 style={{ marginTop: '10vh', color: dark ? text.darkHigh : text.lightHigh }}>My Projects:</h2>
 			<ListProjects dark={dark} className='align-items-center' feat={true} />
+			<div className='row justify-content-end mb-5 pe-5'>
+				<button className='btn btn-primary col-6 col-md-2 col-xxl-1'>
+					<Link
+						to={{ pathname: '/projects' }}
+						className='nope'
+						style={{ color: 'inherit', textDecoration: 'none' }}
+					>
+						See More ↗
+					</Link>
+				</button>
+			</div>
 
 			<Testimonials dark={dark} className='mb-5' />
 		</Fragment>
