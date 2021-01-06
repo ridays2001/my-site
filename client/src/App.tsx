184d63f8c4b2f57914b880ae3b6187cbe02b7ad9
Riday ♡ <ridayshah2001@gmail.com>
2021-01-06 17:08:50 +0530

    Feat: added footer to main component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ThemeContext from './assets/theme/themeContext';
+import Footer from './components/footer';
 import routes from './util/routes';
 
 const App = () => {
+	const { dark } = useContext(ThemeContext);
 	return (
 		<div className='App justify-content-center text-center'>
 			<Switch>
@@ -10,6 +13,7 @@ const App = () => {
 					<Route {...route} />
 				))}
 			</Switch>
+			<Footer dark={dark} />
 		</div>
 	);
 };
