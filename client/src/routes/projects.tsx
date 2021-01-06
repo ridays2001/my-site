7fd037e5ff8d33c497253e8074cd5c2bd2c59c25
Riday ♡ <ridayshah2001@gmail.com>
2021-01-06 13:08:12 +0530

    Feat: filled projects page

--- client/src/routes/projects.tsx
+++ client/src/routes/projects.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+import ThemeContext from '../assets/theme/themeContext';
+import Nav from '../components/nav';
+import ListProjects from '../components/listProjects';
+import { text } from '../assets/theme/constants';
 
-const Projects = () => <div>My Projects Page</div>;
+const Projects = () => {
+	const { dark } = useContext(ThemeContext);
+	return (
+		<Fragment>
+			<Nav active='projects' />
+			<h2 style={{ color: dark ? text.darkHigh : text.lightHigh, marginTop: '5vh' }}>My Notable Projects:</h2>
+			<ListProjects feat={false} dark={dark} className='align-items-center' />
+		</Fragment>
+	);
+};
 
 export default Projects;
