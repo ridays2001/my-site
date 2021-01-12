bdf79fe9c8902b19c3c673499d070a0bfd74ca9b
Riday ♡ <ridayshah2001@gmail.com>
2021-01-12 18:50:18 +0530

    Feat: router for testimonials section

---
+++ server/src/routers/testimonials.ts
@@ -0,0 +1,47 @@
+import rateLimit from 'express-rate-limit';
+import db from '../util/db';
+import type { Testimonial } from '../util/schemas';
+
+import express from 'express';
+const router = express.Router();
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.get('/', async (_req, res) => {
+	const testimonials: Array<Testimonial> = [];
+	await db
+		.db('main')
+		.collection<Testimonial>('testimonials')
+		.find()
+		.toArray()
+		.then(reviews => testimonials.push(...reviews));
+
+	return res.json(testimonials);
+});
+
+// 1 max testimonial over the duration of 24 hours.
+const limiter = rateLimit({
+	max: 1,
+	windowMs: 24 * 60 * 60 * 1000,
+	message: 'Please do not abuse this service and provide genuine testimonials only.',
+	headers: true
+});
+
+router.post('/', limiter, (req, res) => {
+	if (req.headers.authorization !== process.env.AUTH) return res.status(501).end('Denied!');
+
+	const { message, name, rating } = req.body as Testimonial;
+
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+	if (!message?.length || !name?.length || !rating) {
+		return res.status(502).end('Incomplete Body.');
+	}
+
+	void db
+		.db('main')
+		.collection<Testimonial>('testimonials')
+		.insertOne({ message, name, rating: Number(rating) });
+
+	return res.end('Saved.');
+});
+
+export default router;
