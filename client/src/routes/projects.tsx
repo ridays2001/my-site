c65619aeb9a59024e94bf15ef6190857d8620d67
Riday ♡ <ridayshah2001@gmail.com>
2021-01-28 17:17:47 +0530

    Feat: set page title for projects

--- client/src/routes/projects.tsx
+++ client/src/routes/projects.tsx
@@ -1,10 +1,15 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import ThemeContext from '../assets/theme/themeContext';
 import ListProjects from '../components/listProjects';
 import Nav from '../components/nav';
 
 const Projects = () => {
 	const { dark } = useContext(ThemeContext);
+
+	useEffect(() => {
+		document.title = 'My Projects | Riday Shah';
+	}, []);
+
 	return (
 		<Fragment>
 			<Nav active='projects' />
