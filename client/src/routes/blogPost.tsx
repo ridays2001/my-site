f5535fb74209215ea4b1a07b5ae4622658e06580
Riday ♡ <ridayshah2001@gmail.com>
2021-01-28 17:52:34 +0530

    Feat: blog post component

---
+++ client/src/routes/blogPost.tsx
@@ -0,0 +1,101 @@
+import React, { Fragment, useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import PostSection from '../assets/styles/postSection';
+import ThemeContext from '../assets/theme/themeContext';
+import Nav from '../components/nav';
+import type { BlogPost as Post } from '../util/api';
+import { getBlogPost, likePost, unlikePost } from '../util/api';
+import formatDate from '../util/formatDate';
+import { Comment, Like, User } from '../util/icons';
+
+const BlogPost = () => {
+	const { dark } = useContext(ThemeContext);
+	const { id } = useParams<{ id: string }>();
+	const [post, setPost] = useState<Post | undefined>(undefined);
+	const [showComments, setComments] = useState(false);
+	const [isLiked, setLiked] = useState(false);
+	const [likeCount, setCount] = useState(0);
+
+	useEffect(() => {
+		document.title = "Riday's Diary";
+	}, []);
+
+	useEffect(() => {
+		(async () => {
+			const data = await getBlogPost(id).catch(() => undefined);
+			setPost(data);
+
+			if (data) setCount(data.likes);
+			setLiked(Boolean(window.localStorage.getItem(`like-${id}`)));
+		})();
+	}, [id]);
+
+	/* TODO:
+	 * Add option for comments.
+	 * Make a markdown editor.
+	 */
+
+	return (
+		<Fragment>
+			<Nav active='blog' />
+			<h2>Riday's Diary</h2>
+			<PostSection dark={dark ? 1 : 0} className='col-11 col-md-8 col-xl-6 py-4 px-lg-5 mx-auto'>
+				{post && (
+					<article>
+						<h5>{post.title}</h5>
+						<br />
+						<p dangerouslySetInnerHTML={{ __html: post.post }}></p>
+						<section className='col-11 mx-auto card'>
+							<div className='card-title'>
+								<button className='btn-primary' onClick={() => setComments(!showComments)}>
+									<Comment dark={dark} />
+									&ensp;{post.comments.length} Comments
+								</button>
+								&emsp;&emsp;
+								<button
+									className={isLiked ? 'btn-primary' : 'btn-outline-primary'}
+									onClick={() => {
+										setLiked(!isLiked);
+										if (isLiked) {
+											unlikePost(post.id);
+											setCount(likeCount - 1);
+											window.localStorage.removeItem(`like-${id}`);
+										} else {
+											likePost(post.id);
+											setCount(likeCount + 1);
+											window.localStorage.setItem(`like-${id}`, 'true');
+										}
+									}}
+								>
+									<Like dark={dark} />
+									&ensp;{likeCount}
+								</button>
+							</div>
+							{showComments && (
+								<ul className='list-group list-group-flush text-start'>
+									{post.comments.map(({ comment, name, timestamp }) => {
+										return (
+											<li className='list-group-item'>
+												<div className='row mb-3'>
+													<div className='col-md-8 text-start'>
+														<User dark={dark} /> &ensp; {name}
+													</div>
+													<div className='col-md-4 text-md-end' id='time'>
+														{formatDate(timestamp)}
+													</div>
+												</div>
+												<p dangerouslySetInnerHTML={{ __html: comment }} className='mx-3'></p>
+											</li>
+										);
+									})}
+								</ul>
+							)}
+						</section>
+					</article>
+				)}
+			</PostSection>
+		</Fragment>
+	);
+};
+
+export default BlogPost;
