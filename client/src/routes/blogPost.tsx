0597b4c41c1d0153344953143b7894572518b99d
Riday ♡ <ridayshah2001@gmail.com>
2021-02-14 14:14:13 +0530

    Feat: render 404 component if post not found

--- client/src/routes/blogPost.tsx
+++ client/src/routes/blogPost.tsx
@@ -8,6 +8,7 @@ import type { BlogPost as Post } from '../util/api';
 import { getBlogPost, likePost, submitComment, unlikePost } from '../util/api';
 import formatDate from '../util/formatDate';
 import { Comment, Like, User } from '../util/icons';
+import Error from './error';
 
 const BlogPost = () => {
 	const { dark } = useContext(ThemeContext);
@@ -16,11 +17,11 @@ const BlogPost = () => {
 	const [showComments, setComments] = useState(false);
 	const [isLiked, setLiked] = useState(false);
 	const [likeCount, setCount] = useState(0);
-	const [isSubmitted, setSubmitted] = useState(false);
 	const [isNameTouched, touchName] = useState(false);
 	const [isNameValid, validateName] = useState(false);
 	const [isEmailTouched, touchEmail] = useState(false);
 	const [isEmailValid, validateEmail] = useState(false);
+	const [missing, setMissing] = useState(false);
 
 	useEffect(() => {
 		document.title = "Riday's Diary";
@@ -30,7 +31,10 @@ const BlogPost = () => {
 		let mounted = true;
 		(async () => {
 			if (!mounted) return undefined;
-			const data = await getBlogPost(id).catch(() => undefined);
+			const data = await getBlogPost(id).catch(() => {
+				setMissing(true);
+				return undefined;
+			});
 			setPost(data);
 
 			if (data) setCount(data.likes);
@@ -54,118 +58,134 @@ const BlogPost = () => {
 		submitComment(id, name.value, email.value, comment.value)
 			.catch(() => undefined)
 			.then(() => {
-				setSubmitted(true);
 				window.location.reload();
 			});
 	};
 
 	return (
 		<Fragment>
-			<Nav active='blog' />
-			<h2>Riday's Diary</h2>
-			<PostSection dark={dark ? 1 : 0} className='l1 col-11 col-md-8 col-xl-6 py-4 px-lg-5 mx-auto'>
-				{post && (
-					<article>
-						<h5>{post.title}</h5>
-						<br />
-						<p dangerouslySetInnerHTML={{ __html: post.post }}></p>
-						<section className='col-11 mx-auto card l2'>
-							<div className='card-body'>
-								<div className='card-title'>
-									<button className='btn-primary' onClick={() => setComments(!showComments)}>
-										<Comment dark={dark} />
-										&ensp;{post.comments.length} Comments
-									</button>
-									&emsp;&emsp;
-									<button
-										className={isLiked ? 'btn-primary' : 'btn-outline-primary'}
-										onClick={() => {
-											setLiked(!isLiked);
-											if (isLiked) {
-												unlikePost(post.id);
-												setCount(likeCount - 1);
-												window.localStorage.removeItem(`like-${id}`);
-											} else {
-												likePost(post.id);
-												setCount(likeCount + 1);
-												window.localStorage.setItem(`like-${id}`, 'true');
-											}
-										}}
-									>
-										<Like dark={dark} />
-										&ensp;{likeCount}
-									</button>
-								</div>
-								{showComments && (
-									<Fragment>
-										<ul className='list-group list-group-flush text-start'>
-											{post.comments.map(({ comment, name, timestamp }, i) => {
-												return (
-													<li className='list-group-item' key={i}>
-														<div className='row mb-3'>
-															<div className='col-md-8 text-start'>
-																<User dark={dark} /> &ensp; {name}
-															</div>
-															<div className='col-md-4 text-md-end' id='time'>
-																{formatDate(timestamp)}
-															</div>
-														</div>
-														<p
-															dangerouslySetInnerHTML={{ __html: comment }}
-															className='mx-3'
-														></p>
-													</li>
-												);
-											})}
-										</ul>
-
-										<div className='col-11 mx-auto mb-4'>
-											<label htmlFor='name'>Name</label>
-											<input
-												type='text'
-												name='name'
-												id='name'
-												className={`form-control my-2 ${
-													isNameTouched ? (isNameValid ? 'is-valid' : 'is-invalid') : ''
-												}`}
-												onBlur={() => (isNameTouched ? undefined : touchName(true))}
-												onChange={() => {
-													const name = document.getElementById('name') as HTMLInputElement;
-													if (/.{2,}/g.test(name.value)) validateName(true);
-													else validateName(false);
+			{missing && <Error />}
+			{!missing && (
+				<Fragment>
+					<Nav active='blog' />
+					<h2>Riday's Diary</h2>
+					<PostSection dark={dark ? 1 : 0} className='l1 col-11 col-md-8 col-xl-6 py-4 px-lg-5 mx-auto'>
+						{post && (
+							<article>
+								<h5>{post.title}</h5>
+								<br />
+								<p dangerouslySetInnerHTML={{ __html: post.post }}></p>
+								<section className='col-11 mx-auto card l2'>
+									<div className='card-body'>
+										<div className='card-title'>
+											<button className='btn-primary' onClick={() => setComments(!showComments)}>
+												<Comment dark={dark} />
+												&ensp;{post.comments.length} Comments
+											</button>
+											&emsp;&emsp;
+											<button
+												className={isLiked ? 'btn-primary' : 'btn-outline-primary'}
+												onClick={() => {
+													setLiked(!isLiked);
+													if (isLiked) {
+														unlikePost(post.id);
+														setCount(likeCount - 1);
+														window.localStorage.removeItem(`like-${id}`);
+													} else {
+														likePost(post.id);
+														setCount(likeCount + 1);
+														window.localStorage.setItem(`like-${id}`, 'true');
+													}
 												}}
-												required
-											/>
-
-											<label htmlFor='email'>Email</label>
-											<input
-												type='text'
-												name='email'
-												id='email'
-												className={`form-control my-2 ${
-													isEmailTouched ? (isEmailValid ? 'is-valid' : 'is-invalid') : ''
-												}`}
-												onBlur={() => (isEmailTouched ? undefined : touchEmail(true))}
-												onChange={() => {
-													const email = document.getElementById('email') as HTMLInputElement;
-													if (/\w+@\w+[.].+/g.test(email.value)) validateEmail(true);
-													else validateEmail(false);
-												}}
-												required
-											/>
+											>
+												<Like dark={dark} />
+												&ensp;{likeCount}
+											</button>
 										</div>
+										{showComments && (
+											<Fragment>
+												<ul className='list-group list-group-flush text-start'>
+													{post.comments.map(({ comment, name, timestamp }, i) => {
+														return (
+															<li className='list-group-item' key={i}>
+																<div className='row mb-3'>
+																	<div className='col-md-8 text-start'>
+																		<User dark={dark} /> &ensp; {name}
+																	</div>
+																	<div className='col-md-4 text-md-end' id='time'>
+																		{formatDate(timestamp)}
+																	</div>
+																</div>
+																<p
+																	dangerouslySetInnerHTML={{ __html: comment }}
+																	className='mx-3'
+																></p>
+															</li>
+														);
+													})}
+												</ul>
 
-										<MDEditor dark={dark} />
-										<button className='btn-primary mt-3' onClick={handleSubmit}>
-											Add Comment
-										</button>
-									</Fragment>
-								)}
-							</div>
-						</section>
-					</article>
-				)}
-			</PostSection>
+												<div className='col-11 mx-auto mb-4'>
+													<label htmlFor='name'>Name</label>
+													<input
+														type='text'
+														name='name'
+														id='name'
+														className={`form-control my-2 ${
+															isNameTouched
+																? isNameValid
+																	? 'is-valid'
+																	: 'is-invalid'
+																: ''
+														}`}
+														onBlur={() => (isNameTouched ? undefined : touchName(true))}
+														onChange={() => {
+															const name = document.getElementById(
+																'name'
+															) as HTMLInputElement;
+															if (/.{2,}/g.test(name.value)) validateName(true);
+															else validateName(false);
+														}}
+														required
+													/>
+
+													<label htmlFor='email'>Email</label>
+													<input
+														type='text'
+														name='email'
+														id='email'
+														className={`form-control my-2 ${
+															isEmailTouched
+																? isEmailValid
+																	? 'is-valid'
+																	: 'is-invalid'
+																: ''
+														}`}
+														onBlur={() => (isEmailTouched ? undefined : touchEmail(true))}
+														onChange={() => {
+															const email = document.getElementById(
+																'email'
+															) as HTMLInputElement;
+															if (/\w+@\w+[.].+/g.test(email.value)) validateEmail(true);
+															else validateEmail(false);
+														}}
+														required
+													/>
+												</div>
+
+												<MDEditor dark={dark} />
+												<button className='btn-primary mt-3' onClick={handleSubmit}>
+													Add Comment
+												</button>
+											</Fragment>
+										)}
+									</div>
+								</section>
+							</article>
+						)}
+					</PostSection>
+				</Fragment>
+			)}
 		</Fragment>
 	);
 };
