30e0b12a98679bcb6962fcd7742751afcd9e1155
Riday ♡ <ridayshah2001@gmail.com>
2021-03-05 17:03:46 +0530

    Feat: custom webpack chunk names

--- client/src/util/routes.ts
+++ client/src/util/routes.ts
@@ -2,16 +2,16 @@ import type { LazyExoticComponent } from 'react';
 import { lazy } from 'react';
 
 // Lazy loaded components to improve performance.
-const About = lazy(() => import('../routes/about'));
-const Blog = lazy(() => import('../routes/blog'));
-const BlogPost = lazy(() => import('../routes/blogPost'));
-const NewPost = lazy(() => import('../routes/newPost'));
-const BlogUnsubscribe = lazy(() => import('../routes/blogUnsubscribe'));
-const Contact = lazy(() => import('../routes/contact'));
-const Home = lazy(() => import('../routes/home'));
-const NewTestimonial = lazy(() => import('../routes/newTestimonial'));
-const Projects = lazy(() => import('../routes/projects'));
-const Error = lazy(() => import('../routes/error'));
+const About = lazy(() => import(/* webpackChunkName: 'about' */ '../routes/about'));
+const Blog = lazy(() => import(/* webpackChunkName: 'blog' */ '../routes/blog'));
+const BlogPost = lazy(() => import(/* webpackChunkName: 'blogPost' */ '../routes/blogPost'));
+const NewPost = lazy(() => import(/* webpackChunkName: 'newPost' */ '../routes/newPost'));
+const BlogUnsubscribe = lazy(() => import(/* webpackChunkName: 'blogUnsubscribe' */ '../routes/blogUnsubscribe'));
+const Contact = lazy(() => import(/* webpackChunkName: 'contact' */ '../routes/contact'));
+const Home = lazy(() => import(/* webpackChunkName: 'home' */ '../routes/home'));
+const NewTestimonial = lazy(() => import(/* webpackChunkName: 'newTestimonial' */ '../routes/newTestimonial'));
+const Projects = lazy(() => import(/* webpackChunkName: 'projects' */ '../routes/projects'));
+const Error = lazy(() => import(/* webpackChunkName: 'error' */ '../routes/error'));
 
 interface RouteType {
 	exact?: boolean;
